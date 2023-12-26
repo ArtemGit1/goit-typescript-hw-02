19e35edfad516aaab4a14e48bb7cce1d68245eb2
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-12-26 16:22:47 +0200

    123

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,16 +2,20 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1: {
+type PageDetails = {
+  createAt: Date;
+  updateAt: Date;
+};
+
+type Page = {
   title: string;
   likes: number;
   accounts: string[];
-  status: string;
-  details?: {
-    createAt: Date;
-    updateAt: Date;
-  }
-} = {
+  status: 'open' | 'close';
+  details?: PageDetails;
+};
+
+const page1: Page = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -19,22 +23,15 @@ const page1: {
   details: {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
-  }
-}
+  },
+};
 
-const page2: {
-  title: string;
-  likes: number;
-  accounts: string[];
-  status: string;
-  details?: {
-    createAt: Date;
-    updateAt: Date;
-  }
-} = {
+
+const page2 = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
-}
+};
+
 export {};
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -10,14 +10,13 @@ type AllType = {
   color: string;
   weight: number;
 };
+
 function compare<T extends keyof AllType>(top: Pick<AllType, T>, bottom: Pick<AllType, T>): Pick<AllType, T> {
-  const result = {} as Pick<AllType, T>;
-  Object.keys(top).forEach(key => {
-    result[key as keyof AllType] = top[key as keyof AllType];
-  });
-  Object.keys(bottom).forEach(key => {
-    result[key as keyof AllType] = bottom[key as keyof AllType];
-  });
-  return result;
+  return {
+    ...top,
+    ...bottom,
+  };
 }
+
 export {};
+
